8ea1c3bb5fbeaa03a25d33e710652aebacf79273
erikdeirdre <pwhite@delpwhite.org>
2023-03-16 19:43:09 -0400

    more work

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 """ Main Module """
 from sys import (exit, stdout)
-from os import getenv
+from os import getenv, path
 import json
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from helpers.utils import HttpError
 
@@ -18,38 +19,85 @@ logging.info('Starting ...')
 
 CORS(APP, resources={r"/*": {"origins": "*", "send_wildcard": "False"}})
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in APP.config['ALLOWED_EXTENSIONS']
 
+def check_file(request, ke, upload_dir):
+    try:
+        temp_file = request.files[ke]
+        if temp_file.filename == '':
+            msg = f"{ke} schedule file missing"
+            resp = jsonify({"message": msg})
+            resp.status_code = 500
+            logging.error(msg)
+            return None, resp
+    except KeyError:
+        msg = f"Missing {ke} file"
+        resp = jsonify({"message": msg})
+        resp.status_code = 500
+        logging.error(msg)
+        return None, resp
+
+    if allowed_file(temp_file.filename):
+        filename = secure_filename(temp_file.filename)
+        temp_name = path.join(upload_dir, filename)
+        try:
+            temp_file.save(temp_name)
+        except Exception as e:
+            msg = f"{e.strerror}, {temp_name}"
+            resp = jsonify({"message": msg})
+            resp.status_code = 500
+            logging.error(msg)
+            return None, resp
+    else:
+        msg = f"Invalid File extension for {temp_file.filename}. Accepted type is 'xlsx'."
+        resp = jsonify({"message": msg})
+        resp.status_code = 500
+        logging.error(msg)
+        return None, resp  
+
+    return temp_file, None
+        
 @APP.errorhandler(HttpError)
 def handle_auth_error(ex):
     response = jsonify(ex.error)
     response.status_code = ex.status_code
     return response
 
-@APP.route('/api/health')
+@APP.route('/health')
 def health():
     return 'healthy'
 
-@APP.route('/api/contact', methods=['POST'])
-def contact():
-    try:
-        response = email_client.send_email(request.get_json(), "contact.html", True)
-        resp = jsonify(response['message'])
-        resp.status_code = response['code']
-        return resp
-    except Exception as e:
-        resp = jsonify({"message": "Error processing Contact Form"})
-        resp.status_code = 500
-        logging.error(e)
+@APP.route('/', methods=['GET'])
+def main_page():
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data action="http://localhost:5000/upload">
+      <input type=file name=main>
+      <input type=file name=team>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
-@APP.route('/api/profile', methods=['POST'])
-@require_auth(None)
-def update_profile():
-    try:
-        profile = management_api.update_profile(json.loads(request.data))
-        resp = jsonify(profile['message'])
-        resp.status_code = profile['code']
-    except Exception as e:
-        resp = jsonify({ "message": f"{e}"})
-        resp.status_code = 406
+@APP.route('/upload', methods=['POST'])
+def upload_main_file():
+    if request.method == 'POST':
+        main_file, resp = check_file(request, 'main', APP.config['UPLOAD_FOLDER'])
+        if main_file is None:
+            return resp
+        
+        team_file, resp = check_file(request, 'team', APP.config['UPLOAD_FOLDER'])
+        if team_file is None:
+            return resp
+        
+        if main_file.filename == team_file.filename:
+            msg = "Main schedule and Team schedule are the same name."
+            resp = jsonify({"message": msg})
+            resp.status_code = 500
+            logging.error(msg)
+            return resp
 
-    return resp
+        print('process file')          
--- app/settings.py
+++ app/settings.py
@@ -28,24 +28,5 @@ SECURITY_USER_IDENTITY_ATTRIBUTES = 'email'
 SECURITY_LOGIN_URL = '/login'
 WTF_CSRF_ENABLED = False
 
-# Email
-MAIL_SERVER = getenv('MAIL_SERVER', 'smtp.sendgrid.net')
-MAIL_PORT = int(getenv('MAIL_PORT', '587'))
-MAIL_USE_TLS = getenv('MAIL_USE_TLS', True)
-MAIL_USE_SSL = getenv('MAIL_USE_SSL', False)
-MAIL_USERNAME = getenv('MAIL_USERNAME')
-MAIL_PASSWORD = getenv('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = getenv('MAIL_DEFAULT_SENDER')
-
-# Admin account
-ADMIN_PASSWORD = getenv('ADMIN_PASSWORD', 'password123#')
-ADMIN_EMAIL = getenv('ADMIN_EMAIL', 'admin@example.com')
-EMAIL_SUBJECT_PREFIX = f"[{APP_NAME}]"
-EMAIL_SENDER = f"{APP_NAME} Admin <{MAIL_USERNAME}>"
-
-SOCIAL_GOOGLE = {
-    'consumer_key': "cute.apps.googleusercontent.com",
-    'consumer_secret': "password"
-}
-
-GRAPHIQL = getenv('GRAPHIQL', False)
+UPLOAD_FOLDER = getenv('UPLOAD_FOLDER') or 'uploads'
+ALLOWED_EXTENSIONS = {'xlsx', 'txt'}

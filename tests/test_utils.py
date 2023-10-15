af205c8d7e06ae5085b2d72447ff5166e6751acf
erikdeirdre <pwhite@delpwhite.org>
2023-10-14 21:06:59 -0400

    remove tests directory

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from helpers.utils import HttpError
-
-
-class TestHttpError(unittest.TestCase):
-    def test_http_error(self):
-        http_error = HttpError("This is an error", 500)
-        self.assertEqual(http_error.error, "This is an error")
-        self.assertEqual(http_error.status_code, 500)

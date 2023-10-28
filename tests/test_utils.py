5b0ae6a700a40ae3808204ed7ce4de0fb4459ca0
erikdeirdre <pwhite@delpwhite.org>
2023-10-27 22:56:27 -0400

    add tests for get_environment

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,14 @@
-import unittest
+from unittest import (TestCase, mock)
+from os import environ
 from os.path import (join, dirname)
-from schedule.helpers.utils import (get_arguments, load_translation_file)
+from schedule.helpers.utils import (get_arguments, load_translation_file,
+                                    get_environment)
 
 USAGE='USAGE: schedule.py -s <town schedule file> -t <town>' 
 
 TOWN_NAME = 'town name'
 
-class TestGetArguments(unittest.TestCase):
+class TestGetArguments(TestCase):
     def test_help(self):
         expected_args = {
             'town_file': None, 'town': None
@@ -15,6 +17,12 @@ class TestGetArguments(unittest.TestCase):
             rc, args = get_arguments(['-h'])
         self.assertEqual(cm.output, [f"ERROR:root:{USAGE}"])
         self.assertEqual(rc, 99)
+        self.assertEqual(args, expected_args)
+
+    def test_valid_options(self):
+        expected_args = {'town_file': 'test_file', 'town': 'test'}
+        rc, args = get_arguments(['-t', 'test', '-s', 'test_file'])
+        self.assertEqual(rc, 0)
         self.assertEqual(args, expected_args)
 
     def test_invalid_options(self):
@@ -42,7 +50,88 @@ class TestGetArguments(unittest.TestCase):
         self.assertEqual(args, expected_args)
 
 
-class TestLoadTransactionFile(unittest.TestCase):
+class TestGetEnvironment(TestCase):
+    @mock.patch.dict(environ,{
+    "MASTER_SCHEDULE_ID": "schedule_id",
+    "RANGE_NAME": "range_name",
+    "OUTPUT_FILE_PREFIX": "file_prefix",
+    "GOOGLE_APPLICATION_CREDENTIALS": "dontshare",
+    "TRANSLATION_FILE": "translation_file"
+    }, clear=True)
+    def test_all_variables_set(self):
+        expected_result = {
+            'spreadsheet_id': 'schedule_id',
+            'range_name': 'range_name',
+            'translation_file': 'translation_file',
+            'output_file_prefix': 'file_prefix',
+            'credentials': 'dontshare'
+        }
+        rc, result = get_environment()
+        self.assertEqual(rc, 0)
+        self.assertEqual(result, expected_result)
+
+    @mock.patch.dict(environ,{
+    "RANGE_NAME": "range_name",
+    "OUTPUT_FILE_PREFIX": "file_prefix",
+    "GOOGLE_APPLICATION_CREDENTIALS": "dontshare",
+    "TRANSLATION_FILE": "translation_file"
+    }, clear=True)
+    def test_missing_master_id(self):
+        expected_result = {
+            'range_name': 'range_name',
+            'translation_file': 'translation_file',
+            'output_file_prefix': 'file_prefix',
+            'credentials': 'dontshare'
+        }
+
+        with self.assertLogs(level='INFO') as cm:
+            rc, result = get_environment()
+        self.assertEqual(rc, 66)
+        self.assertEqual(cm.output,
+                         ['ERROR:root:MASTER_SCHEDULE_ID environment variable is missing'])
+        self.assertEqual(result, expected_result)
+
+    @mock.patch.dict(environ,{
+    "MASTER_SCHEDULE_ID": "schedule_id",
+    "RANGE_NAME": "range_name",
+    "GOOGLE_APPLICATION_CREDENTIALS": "dontshare",
+    }, clear=True)
+    def test_default_values(self):
+        expected_result = {
+            'spreadsheet_id': 'schedule_id',
+            'range_name': 'range_name',
+            'translation_file': 'files/translations.json',
+            'output_file_prefix': 'schedule',
+            'credentials': 'dontshare'
+        }
+
+        with self.assertLogs(level='INFO') as cm:
+            rc, result = get_environment()
+        self.assertEqual(rc, 0)
+        self.assertEqual(cm.output,
+                         ['WARNING:root:TRANSLATION_FILE environment variable is missing, using default',
+                          'WARNING:root:OUTPUT_FILE_PREFIX environment variable is missing, using default'])
+        self.assertEqual(result, expected_result)
+
+    def test_missing_everything(self):
+        expected_result = {
+            'translation_file': 'files/translations.json',
+            'output_file_prefix': 'schedule'
+        }
+
+        with self.assertLogs(level='INFO') as cm:
+            rc, result = get_environment()
+        self.assertEqual(rc, 66)
+        self.assertEqual(cm.output,
+                         ['ERROR:root:MASTER_SCHEDULE_ID environment variable is missing',
+                          'ERROR:root:RANGE_NAME environment variable is missing',
+                          'WARNING:root:TRANSLATION_FILE environment variable is missing, using default',
+                          'WARNING:root:OUTPUT_FILE_PREFIX environment variable is missing, using default',
+                          'ERROR:root:GOOGLE_APPLICATION_CREDENTIALS environment variable is missing'])
+        self.assertEqual(result, expected_result)
+
+
+class TestLoadTransactionFile(TestCase):
     def test_valid_file(self):
         expected_json = {
             "age_groups": {
